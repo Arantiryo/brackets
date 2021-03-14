6609fb87546a35005978d4387a46338fe4bcff89
Ivan Tsenilov <arantirex@gmail.com>
2021-03-14 17:17:21 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //let brackets = "[]{}()<>"
+  //let brackets = bracketsConfig.reduce( (a,b) => String(a) + String(b) ).replaceAll(',','');
+  let brackets = '';
+  for (let i in bracketsConfig) {
+  	for (let j in bracketsConfig[i]) {
+    	brackets += bracketsConfig[i][j];
+    }
+  }
+  let stack = [];
+
+  for (let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket);
+
+    if (bracketsIndex % 2 === 0 ) {
+        stack.push(bracketsIndex + 1);
+    } else {
+      if(stack.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+  console.log(stack);
+  return stack.length === 0;
 }
